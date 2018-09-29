4687ba3715b4f3fa16bba3b066dc7dbe1e6e2acf
Manuel Le Bon <manuellebon@manuels-macbook-pro.local>
2018-09-29 10:57:33 +0200

    Initial Commit

---
+++ Black snapped/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Black snapped
+//
+//  Created by Manuel Le Bon on 29/09/18.
+//  Copyright © 2018 Emanuele Villani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
